0ab29b132155c4120a98ba39ac186f8672168b0e
Lars Tørnes Hansen <devel@larsth.dk>
2015-11-04 15:58:55 +0100

    Fixed GOOS=arm typo, and where the go tool is invoked from: 	modified:   ../../release.bash

--- release.bash
+++ release.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir -p ./release
-env CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags=-d -o ./release/rmsgnogpsd-linux-i386 ./cmd/rmsgnogpsd/rmsgnogpsd.go
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags=-d -o ./release/rmsgnogpsd-linux-amd64 ./cmd/rmsgnogpsd/rmsgnogpsd.go
-env CGO_ENABLED=0 GOOS=linux GOARCH=ARM GOARM=6 go build -ldflags=-d -o ./release/rmsgnogpsd-linux-ARM6 ./cmd/rmsgnogpsd/rmsgnogpsd.go
-env GOOS=windows GOARCH=386 go build -ldflags=-d -o ./release/rmsgnogpsd-windows-i386.exe ./cmd/rmsgnogpsd/rmsgnogpsd.go
-env GOOS=windows GOARCH=amd64 go build -ldflags=-d -o ./release/rmsgnogpsd-windows-amd64.exe ./cmd/rmsgnogpsd/rmsgnogpsd.go
+cd ./cmd/rmsgnogpsd
+env CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags=-d -o ../../release/rmsgnogpsd-linux-i386 ./rmsgnogpsd.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags=-d -o ../../release/rmsgnogpsd-linux-amd64 ./rmsgnogpsd.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm ARM=6 go build -ldflags=-d -o ../../release/rmsgnogpsd-linux-ARM6 ./rmsgnogpsd.go
+env GOOS=windows GOARCH=386 go build -o ../../release/rmsgnogpsd-windows-i386.exe ./rmsgnogpsd.go
+env GOOS=windows GOARCH=amd64 go build -o ../../release/rmsgnogpsd-windows-amd64.exe ./rmsgnogpsd.go

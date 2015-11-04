2635c7753fc64157a0aa1ff95ccf0a31b8a7c5f9
Lars Tørnes Hansen <devel@larsth.dk>
2015-11-04 16:40:50 +0100

    Added generation of SHA256SUM, and SHA512SUM to release.bash: 	modified:   ../release.bash

--- release.bash
+++ release.bash
@@ -7,3 +7,7 @@ env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags=-d -o ../../release/
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm ARM=6 go build -ldflags=-d -o ../../release/rmsgnogpsd-linux-ARM6 ./rmsgnogpsd.go
 env GOOS=windows GOARCH=386 go build -o ../../release/rmsgnogpsd-windows-i386.exe ./rmsgnogpsd.go
 env GOOS=windows GOARCH=amd64 go build -o ../../release/rmsgnogpsd-windows-amd64.exe ./rmsgnogpsd.go
+cd ../../release
+sha256sum -b * > SHA256SUMS
+sha512sum -b * > SHA512SUMS
+cd ..
